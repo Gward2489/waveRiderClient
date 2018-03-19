1c60b86370fa83590e5bfc734fc855fc4c9235d3
Gward2489 <gww8804@gmail.com>
2018-03-19 11:36:39 -0500

    populating beaches in array

--- app/surfReports/controllers/LandingPageCtrl.js
+++ app/surfReports/controllers/LandingPageCtrl.js
@@ -2,4 +2,17 @@ angular
 .module("WaveRiderApp")
 .controller("LandingPageCtrl", function($scope, $location, SurfFactory) {
 
+
+    $scope.beachArray = []
+    if (SurfFactory.beachCache === null)
+    {
+        SurfFactory.getAllSpots()
+        .then(results => {
+            console.log(SurfFactory.beachCache)
+            $scope.beachArray = SurfFactory.beachCache
+        })
+    }
+
+    
+
 })
--- app/surfReports/factories/SurfFactory.js
+++ app/surfReports/factories/SurfFactory.js
@@ -2,4 +2,19 @@ angular
 .module("WaveRiderApp")
 .factory("SurfFactory", function ($http) {
 
+    return Object.create(null, {
+        "beachCache": {
+            value: null,
+            enumerable: true,
+            writable: true
+        },
+        "getAllSpots": {
+            value: function () {
+                return $http.get("http://localhost:5000/api/SurfSpots")
+                    .then(results =>{
+                        this.beachCache = results.data
+                    })
+            }
+        },
+    })
 })
