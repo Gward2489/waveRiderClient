ede79eb70ff2a8364fe1ace09a308e3cba60419a
Gward2489 <gww8804@gmail.com>
2018-03-19 18:44:59 -0500

    initialzing object and creating arrays for report calculations

--- app/surfReports/factories/ReportFactory.js
+++ app/surfReports/factories/ReportFactory.js
@@ -5,11 +5,46 @@ angular
     return Object.create(null, {
         "composeCurrentReport": {
             value: function (resultsArray) {
+                let currentSurfReport = {
+                    "beachName": false,
+                    "averageWavePeriod": false,
+                    "averageDominantWavePeriod": false,
+                    "averageSignificantWaveHeight": false,
+                    "averageSteepness": false,
+                    "averageSwellDirection": false,
+                    "averageSwellHeight": false,
+                    "averageSwellPeriod": false,
+                    "averageWaveDirection": false,
+                    "averageWindWaveDirection": false,
+                    "averageWindWaveHeight": false,
+                    "averageWindWavePeriod": false,
+                    "averageDewPointTemp": false,
+                    "averageGustSpeed": false,
+                    "averageSeaSurfaceTemperature": false,
+                    "averageWindSpeed": false
+                }
+
+                let wavePeriods = []
+                let dominantWavePeriods = []
+                let significantWaveHeights = []
+                let steepnessReadings = []
+                let swellDirections = []
+                let swellHeights = []
+                let swellPeriods = []
+                let waveDirections = []
+                let windWaveDirections = []
+                let windWavePeriods = []
+                let dewPointTemps = []
+                let gustSpeeds = []
+                let seaSurfaceTemps = []
+                let windSpeeds = []
+                
                 resultsArray.foreach(br => {
-                    
+
+
+
                 })
             }
         }
     })
-
 })
